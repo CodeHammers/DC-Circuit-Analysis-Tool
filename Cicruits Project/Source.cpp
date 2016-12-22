03c2ad92afbf1ad899c9fd6158cda0626c728680
Kareem <Kareem>
2016-12-22 23:32:11 +0200

    added library

--- Cicruits Project/Source.cpp
+++ Cicruits Project/Source.cpp
@@ -1,1 +1,13 @@
 //main function goes here
+#include <iostream>
+#include "armadillo"
+using namespace arma;
+using namespace std;
+
+int main() {
+	//example
+	mat x;
+	x << 1 << 2 << endr << 3 << 4;
+	cout << x;
+	cin.get();
+}
