ad48b7c8802938cc6f4039e4347e18d3a59e750c
SayedAlesawy <Sayed.Kotb97@eng-st.cu.edu.eg>
2016-12-22 18:48:10 +0200

    initializing project
    Decalring structs, creating headers and cpp files

---
+++ Cicruits Project/Cicruits Project/Components.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Libraries.h"
+
+struct Component
+{
+	float Magnitude;
+	string Label;
+	int Terminal1;
+	int Terminal2;
+};
+
+
+struct Node
+{
+	int Number;
+	vector<Component>Resistors;
+	vector<Component>CurrentSource;
+	vector<Component>VoltageSource;
+};
