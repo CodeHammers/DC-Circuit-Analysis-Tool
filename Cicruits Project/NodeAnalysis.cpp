0697f93507732818d7c8af9fcb85de656b431444
Kareem <Kareem>
2016-12-22 21:31:30 +0200

    Added more helper method For G

--- Cicruits Project/NodeAnalysis.cpp
+++ Cicruits Project/NodeAnalysis.cpp
@@ -1,16 +1,23 @@
 #include "Components.h"
 
 
-bool checkEssential(Node* node) {
+bool CheckEssential(Node* node) {
 	int totalSize = node->Resistors.size() + node->CurrentSource.size() + node->VoltageSource.size();
 	if (totalSize < 2)
 		return true;
 	return false;
 
 }
-double calculateG(Node* node) {
+double CalculateG(Node* node) {
 	double TotalG = 0;
 	for (std::vector<Component>::iterator it = node->Resistors.begin(); it != node->Resistors.end(); ++it) 
 		TotalG += 1/it->Magnitude;
+	return TotalG;
+}
+double CalculateMutualG(Node* node1,Node* node2) {
+	double TotalG = 0;
+	for (std::vector<Component>::iterator it = node1->Resistors.begin(); it != node1->Resistors.end(); ++it)
+		if( (it->Terminal1==node1->Number&&it->Terminal2 == node2->Number) || (it->Terminal2 == node1->Number&&it->Terminal1 == node2->Number) )
+			TotalG += 1 / it->Magnitude;
 	return TotalG;
 }
